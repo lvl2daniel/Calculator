a91c1209f9c6cef52b9f855881f8831925a2c8ca
Daniel Gonzalez <Danielgonzalez4444@yahoo.com>
2023-01-13 13:51:38 -0500

    Create Decimal and Buggy Calculator Logic

--- main.js
+++ main.js
@@ -29,6 +29,8 @@ function operate(operator, a, b)
         return add(a,b);
     else if(operator === '-')
         return subtract(a,b);
+    else if(operator === null)
+        return 'Error';
 }
 
 function populateDisplay(displayNo)
@@ -45,14 +47,13 @@ populateDisplay(0);
 let firstNumber;
 const container = document.querySelector('#calcContainer')
 const buttons = container.getElementsByClassName("btn");
-let i = 0;
 let last = 0;
 let curr;
 let currentOp;
 let stored;
 let isOperating = false;
 let input;
-for (i = 0; i<buttons.length; i++)
+for (let i = 0; i<buttons.length; i++)
     {
         buttons[i].addEventListener("click", (event) => {
             if (parseInt(event.target.textContent))
@@ -71,11 +72,72 @@ for (i = 0; i<buttons.length; i++)
 
 function calculate(currNo, lastNo, operating)
 {
-
-
+    //vvvvv What's left for this function vvvvv
+    //      Need to round decimal to 2 places ONLY IF its too long.
+    //      Need to clear number on button press if operation already happened
+    //      Need to clear the 0 appended to the start of the calculator.
+    if (currNo === 'C')
+    {
+        last = 0;
+        currentOp = 0;
+        stored = 0;
+        isOperating = false;
+        return 0;
+    }
+    else if ((currNo === 'X') || (currNo ==='%') || (currNo ==='-')  || (currNo ==='+'))
+    {
+        if (operating === true)
+        {
+            stored = operate(currentOp, parseInt(stored), parseInt(lastNo));
+            currentOp = currNo;
+            last = 0;
+            return stored;
+        }
+        else if (operating === false)
+        {
+            stored = last;
+            last = 0;
+            isOperating = true;
+            currentOp = currNo;
+            return currNo;
+        }
+    }
+    else if (currNo === '=')
+    {
+        if (operating === false)
+        {
+            last = 0;
+            stored = 0;
+            currentOp = null;
+            return 'Error'
+        }
+        last = operate(currentOp, parseFloat(stored), parseFloat(lastNo))
+        isOperating = false;
+        return last;
+    }
+    else if (currNo === '.')
+    {
+        if (parseFloat(last) % 1 != 0)
+        {
+            last = 0;
+            stored = 0;
+            currentOp = null;
+            return 'Error'
+        }
+        else
+        {
+            last = concat(lastNo, currNo)
+            return last;
+        }
+    }
+    else
+    {
+        last = concat(lastNo, currNo)
+        return last;
+    }
 }
 
 function concat(a, b)
 {
-    return ("" + a + b);
+    return ("" + a + b)
 }
